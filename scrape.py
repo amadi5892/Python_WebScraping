1aab5bc574d6d9c945fe0ba122ccc7fd45a34840
Parrish Williamson <williamsonparrish@gmail.com>
2020-11-18 10:02:23 -0500

    grabbing a title

---
+++ scrape.py
@@ -0,0 +1,18 @@
+import requests
+import bs4
+
+# ---- GRABBING A TITLE ----
+# result = requests.get('http://www.example.com')
+# # print(result.text)
+
+# # I specifically have to use html5lib as my parser (for Pure Python)
+# soup = bs4.BeautifulSoup(result.text,"html5lib")
+# print(soup)
+
+# print(soup.select('title')[0].getText())
+
+# site_paragraphs = soup.select('p')
+# print(site_paragraphs[0].getText())
+
+
+# ---- GRABBING A CLASS ----
